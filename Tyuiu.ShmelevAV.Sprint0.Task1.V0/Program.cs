9d2eee3a9fb8f3ad6521e96bd48e32d340be56d2
Алексей Шмелев <Tyuiu.shmelevav@outlook.com>
2024-09-12 23:58:04 +0500

    Task1

---
+++ Tyuiu.ShmelevAV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Tyuiu.ShmelevAV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            string name = "Alexey.";
+            
+            int age = 21;
+
+            Console.WriteLine("Hi, my name is " + name + " I'm " + age + " years old!");
+            
+            Console.ReadKey();
+        }
+    }
+}
